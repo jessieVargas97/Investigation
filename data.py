88d5e8f1d38a3d7795f0c655b110959fdd7e7be8
jessieVargas97 <54820385+jessieVargas97@users.noreply.github.com>
2023-02-10 16:13:23 -0500

    final changes

--- data.py
+++ data.py
@@ -48,12 +48,9 @@ mac_assoc = []
 def obtenerMAC(Ap_credentials,cm):
     try:
         connect = nk.ConnectHandler(**Ap_credentials)
-        # Ap_data = connect.send_command("iwinfo wlan1-1 assoc") 
         Ap_data = connect.send_command(cm) #valida cada interfaz activa en los routers    
-        # lista_MAC = Ap_data.splitlines()[0:2:1]
         lista_MAC = Ap_data.splitlines() 
         for i in range(len(lista_MAC)):
-            # listaprueba=lista_MAC[0].split(' ')[0:i+5:1] #ant
             listaprueba=lista_MAC[i].split(' ')
             if len(listaprueba[0]) == 17:
                 mac_assoc.append(listaprueba[0])
@@ -62,13 +59,7 @@ def obtenerMAC(Ap_credentials,cm):
         print(ex)
     return mac_assoc
 
-# t1 = threading.Thread(target=obtenerMAC) #to validate thread
-#change it all per threading start 
-obtenerMAC(Ap3_credentials,"iwinfo wlan1-1 assoc") #no need an arg per comand 
-# R2 = obtenerMAC(Ap1_credentials,"iwinfo wlan1-1 assoc")
-# R3 = obtenerMAC(Ap2_credentials,"iwinfo wlan1-1 assoc")
-
-#validation per macs 
+obtenerMAC(Ap3_credentials,"iwinfo wlan1-1 assoc")
 
 valuesHorst = []
 valorAP = []
@@ -78,19 +69,13 @@ try:
                             user="test",
                             password="H8bmbfar!",
                             db = "wificrowdspy")
-    
-    # dataDate = datetime.now() #objetos se trabajan en en tipo date
-            
-    # limitSupF = dataDate.strftime("%Y-%m-%d %H:%M:%S")
-    # limteInf = dataDate - relativedelta(minutes=50) #se modifiocó por un margen de 10 minutos
-    # limteInfF = limteInf.strftime("%Y-%m-%d %H:%M:%S")
 
     with conn.cursor() as cursor:
         for mac in mac_assoc:
-            dataDate = datetime.now() #objetos se trabajan en en tipo date
-            
+            dataDate = datetime.now()
+            #time lapse check in realtime scenario
             limitSupF = dataDate.strftime("%Y-%m-%d %H:%M:%S")
-            limteInf = dataDate - relativedelta(minutes=50) #se modifiocó por un margen de 10 minutos
+            limteInf = dataDate - relativedelta(minutes=10) 
             limteInfF = limteInf.strftime("%Y-%m-%d %H:%M:%S")
             cursor.execute("SELECT id_router, avg(rssi) FROM info_horst WHERE mac = '5A:B3:F5:94:66:10' and fecha between '2023-02-8 15:33:00' and '2023-02-8 16:03:00' group by id_router order by id_router;")
             # cursor.execute("SELECT id_router, avg(rssi) FROM info_horst WHERE mac = %s and fecha between %s and %s group by id_router order by id_router;",(mac,limteInfF,limitSupF))
@@ -110,10 +95,11 @@ macPerQ = []
 idRouterPerQ = []
 rssivalPerQ = []
 
-#obtengo la mac 
+#obtengo el valor de RSSI   
 for fila in valuesHorst:
-    for elemento in fila:
-        rssivalPerQ.append(elemento[1])
+    if len(fila) == 3:
+        for elemento in fila:
+            rssivalPerQ.append(elemento[1])
         
 #val pos change struct
 for pos in valorAP:
@@ -130,18 +116,16 @@ for pos in valorAP:
 #----------------------------POSICIONAMIENTO----------------------------#
 
 x, y = symbols("x y")
-# x = Symbol('x')
-# y = Symbol('y')
 
 rssi0 = -41.70 #indoors model 40 test 30
 d0 = 1 #ant 0.5
 
 n = 1.54 #ant 2 later 4 
-wl = 2 #ant 2
+wl = 2
 
 #TRILATERACION
 
-rssiT1 = (rssi0-rssivalPerQ[0]+wl)/(10*n) #get values per query result same order?
+rssiT1 = (rssi0-rssivalPerQ[0]+wl)/(10*n) 
 d1 = 10**(rssiT1)*d0 
 rssiT2 = (rssi0-rssivalPerQ[1]+wl)/(10*n)
 d2 = 10**(rssiT2)*d0
@@ -153,76 +137,89 @@ d3 = 10**(rssiT3)*d0
 # d2 = 2.58
 # d3 = 4.16
 
-# f1 = Eq((x-x1)**2 + (y-y1)**2 - d1**2)#ant
 f1 = Eq((((x-x1)**2) + ((y-y1)**2)) , (d1**2))
 f2 = Eq((((x-x2)**2) + ((y-y2)**2)) , (d2**2))
 f3 = Eq((((x-x3)**2) + ((y-y3)**2)) , (d3**2))
 
-# solution = solve((f1,f2,f3),(x,y)) #revisar valor de solucion cruces
+# solution = solve((f1,f2,f3),(x,y)) #revisar SE sol 
 sol1 = solve((f1,f2),(x,y))
 sol2 = solve((f1,f3),(x,y))
-sol3 = solve((f2,f3),(x,y)) #no need
+sol3 = solve((f2,f3),(x,y)) 
 
-#Validar que la solucion es del sistema
 list_posSol = []
 
-#funct?
-# sol1C = list(sol1)
 def solSE(solution):
     lista = list(solution)
     retorno = []
     for i in range(len(lista)):
-    # for s in lista:
         i = i - 1
         if(str(lista[i]).__contains__("I") or str(lista[i]).__contains__("e")):
             lista.remove(lista[i])
-            # i = i - 1
         else:
             retorno.append(lista[i])
-    # valSearched = "I"
-    # lista = filter(lambda val: valSearched in val,solution)
     return retorno
 
-#revisar
 sol1C = solSE(sol1)
-list_posSol.append(sol1C) #revisar
+list_posSol.append(sol1C) 
 sol2C = solSE(sol2)
 list_posSol.append(sol2C)
 sol3C = solSE(sol3)
 list_posSol.append(sol3C)
     
-def parOrdenado(lista1,lista2):
-    if(lista1 )
-
-#Validar dentro dimensiones 
-listVectores = []
-coordperLine = [] #necesario
-limSupX = 4.7
-limInfX = -4.7
-limSupY = 3.5 
-limInfY = -3.5
-for secc in list_posSol:
-    if(len(secc) != 0):
-        coordperLine = str(secc).split(',')
-        # coord = coordperLine
-        for sol in sol1C:
-            for r in sol2C:
-                solstr = str(sol).split(',')
-                rsstr = str(r).split(',')
-                detValX = solstr[0] - rsstr[0]
-                detValY = solstr[1] - rsstr[1]
+def parOrdenado(lista1,lista2,lista3):
+    limSupX = 4.7
+    limInfX = -4.7
+    limSupY = 3.5 
+    limInfY = -3.5
+    listVectoresf = []
+    #procesamiento previo par ordenado ahorro anidaciones?    
+    if not lista1 or not lista2 and not lista1 or not lista3 and not lista2 or not lista3:
+            #revisar logica validaciones NO TIENE
+        for v in lista2:
+            for v1 in lista3:
+                # vstr = str(v).split(',')
+                # v1str = str(v1).split(',')
+                detValX = v[0] - v1[0]
+                detValY = v[1] - v1[1]
                 if(detValX < 0.5 and detValY < 0.5):
-                    # valPromX = (solstr[0] - rsstr[0])/2 #verificar si no funcion
-                    valPromX = np.mean(solstr[0], rsstr[0])
-                    valPromY = np.mean(solstr[1], rsstr[1])
+                    valPromX = np.mean(v[0], v1[0])
+                    valPromY = np.mean(v[1], v1[1])
                     if(valPromX > limSupX or valPromX < limInfX and valPromY > limSupY or valPromY < limInfY):
                         valuetoVector = (valPromX,valPromY)
-                        listVectores.append(valuetoVector) #tiene sentido??
+                        listVectoresf.append(valuetoVector)
+                    # print("Check here. COMPLETE")
+    return listVectoresf
+        
 
-                     
+#Validar dentro dimensiones -> Implementar funcion parOrdenado
+listVectores = []
+# coordperLine = [] #necesario??
+# limSupX = 4.7
+# limInfX = -4.7
+# limSupY = 3.5 
+# limInfY = -3.5
+# for secc in list_posSol:
+#     if(len(secc) != 0):
+#         coordperLine = str(secc).split(',')
+#         # coord = coordperLine
+#         for sol in sol1C:
+#             for r in sol2C:
+#                 solstr = str(sol).split(',')
+#                 rsstr = str(r).split(',')
+#                 detValX = solstr[0] - rsstr[0]
+#                 detValY = solstr[1] - rsstr[1]
+#                 if(detValX < 0.5 and detValY < 0.5):
+#                     # valPromX = (solstr[0] - rsstr[0])/2 #verificar si no funcion
+#                     valPromX = np.mean(solstr[0], rsstr[0])
+#                     valPromY = np.mean(solstr[1], rsstr[1])
+#                     if(valPromX > limSupX or valPromX < limInfX and valPromY > limSupY or valPromY < limInfY):
+#                         valuetoVector = (valPromX,valPromY)
+#                         listVectores.append(valuetoVector) #tiene sentido??
+
+listVectores.append(parOrdenado(sol1C,sol2C,sol3C))              
 
 #----------------------------Distanciamiento----------------------------#
-
+#check other values 
 resultadoDistancia = []
 for i in range(len(listVectores)):
     for j in range(len(listVectores)-1):
@@ -232,4 +229,6 @@ for i in range(len(listVectores)):
 #----------------------------Actualizar BD por posiciones----------------------------#
 
 #agg DB result
-
+#how to identificate per user -> MAC?? (modify query)
+#table_name and attributes
+#relation with the other tables
